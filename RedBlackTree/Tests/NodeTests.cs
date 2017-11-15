4444a7e97b8ecb86360e2c531e21d0e4c2ed9653
ImmortalTurtle <immortalsuperturtle@gmail.com>
2017-11-15 17:54:07 +0300

    Unit tests for RedBlackTree: Node methods and Insertion.

---
+++ RedBlackTree/Tests/NodeTests.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+
+namespace RedBlackTree.Tests
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        [Test]
+        public void Brother()
+        {
+            var root = new Node<int>(5);
+            root.SetLeft(new Node<int>(3));
+            root.GetLeft().parent = root;
+            
+            root.SetRight(new Node<int>(7));
+            root.GetRight().parent = root;
+            
+            Assert.AreEqual(root.GetRight(), root.GetLeft().Brother());
+            
+            root.SetRight(null);
+            
+            Assert.AreEqual(null, root.GetLeft().Brother());
+        }
+        
+        [Test]
+        public void GrandParent()
+        {
+            var root = new Node<int>(5);
+            var left = new Node<int>(3);
+            root.SetLeft(left);
+            left.parent = root;
+            
+            left.SetRight(new Node<int>(4));
+            left.GetRight().parent = left;
+            
+            Assert.AreEqual(root, left.GetRight().GrandParent());
+            Assert.AreEqual(null, root.GrandParent());
+            Assert.AreEqual(null, left.GrandParent());
+
+            left.GetRight().parent = null;
+            
+            Assert.AreEqual(null, left.GetRight().GrandParent());
+        }
+        
+        [Test]
+        public void Uncle()
+        {
+            var root = new Node<int>(5);
+            var left = new Node<int>(3);
+            var right = new Node<int>(6);
+            var leaf = new Node<int>(4);
+            
+            root.SetLeft(left);
+            left.parent = root;
+            
+            root.SetRight(right);
+            root.parent = root;
+            
+            left.SetRight(leaf);
+            leaf.parent = left;
+            
+            Assert.AreEqual(right, leaf.Uncle());
+            
+            root.SetRight(null);
+            right.parent = null;
+            
+            Assert.AreEqual(null, leaf.Uncle());
+
+            leaf.parent = null;
+            
+            Assert.AreEqual(null, leaf.Uncle());
+        }
+
+        [Test]
+        public void RotateLeft()
+        {
+            var expectedTree = new Tree<int>(new Node<int>(20, isBlack:true));
+            expectedTree.root.SetLeft(new Node<int>(15));
+            expectedTree.root.SetRight(new Node<int>(25));
+
+            var actualTree = new Tree<int>();
+            actualTree.root = new Node<int>(15, isBlack: true);
+            actualTree.root.SetRight(new Node<int>(20));
+            actualTree.root.GetRight().SetRight(new Node<int>(25));
+
+            var right = actualTree.root.GetRight();
+            actualTree.root.RotateLeft();
+            actualTree.root.isBlack = false;
+            actualTree.root = right;
+            actualTree.root.isBlack = true;
+            
+            Assert.AreEqual(expectedTree, actualTree);
+        }
+        
+        [Test]
+        public void RotateRight()
+        {
+            var expectedTree = new Tree<int>(new Node<int>(20, isBlack:true));
+            expectedTree.root.SetLeft(new Node<int>(15));
+            expectedTree.root.SetRight(new Node<int>(25));
+
+            var actualTree = new Tree<int>();
+            actualTree.root = new Node<int>(25, isBlack: true);
+            actualTree.root.SetLeft(new Node<int>(20));
+            actualTree.root.GetLeft().SetLeft(new Node<int>(15));
+
+            var left = actualTree.root.GetLeft();
+            actualTree.root.RotateRight();
+            actualTree.root.isBlack = false;
+            actualTree.root = left;
+            actualTree.root.isBlack = true;
+            
+            Assert.AreEqual(expectedTree, actualTree);
+        }
+    }
+}
---
+++ RedBlackTree/Tests/TreeTests.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace RedBlackTree.Tests
+{
+    [TestFixture]
+    public class TreeTests
+    {
+        [Test]
+        public void InsertOneKey() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(20);;
+            root.isBlack = true;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            actualTree.Insert(20);
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+        
+        [Test]
+        public void InsertTwoKeys() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(20);;
+            root.isBlack = true;
+    
+            expectedTree.root = root;
+            expectedTree.root.SetLeft(new Node<int>(15));
+            expectedTree.root.SetRight(new Node<int>(25));
+    
+            var actualTree = new Tree<int>();
+    
+            actualTree.Insert(20);
+            actualTree.Insert(15);
+            actualTree.Insert(25);
+            
+            Console.WriteLine(actualTree.root);
+            var iter = new BfsIterator<int>(actualTree);
+            Console.WriteLine("Tree:");
+            while (iter.hasNext())
+            {
+                Console.WriteLine(iter.next().value);
+            }
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        [Test]
+        public void InsertRightRightCase() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(25);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(20);
+            root.SetLeft(node1);
+    
+            var node2 = new Node<int>(30);
+            root.SetRight(node2);
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+
+    
+            foreach (var i in new List<int>{20, 25, 30})
+            {
+                actualTree.Insert(i);
+            };
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        [Test]
+        public void InsertRightLeftCase() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(25);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(20);
+            root.SetLeft(node1);
+            node1.parent = root;
+    
+            var node2 = new Node<int>(30);
+            root.SetRight(node2);
+            node2.parent = root;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            foreach (var i in new List<int>{20, 30, 25})
+            {
+                actualTree.Insert(i);
+            };
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        [Test]
+        public void InsertLeftLeftCase() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(25);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(20);
+            root.SetLeft(node1);
+            node1.parent = root;
+    
+            var node2 = new Node<int>(30);
+            root.SetRight(node2);
+            node2.parent = root;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            foreach (var i in new List<int>{30, 25, 20})
+            {
+                actualTree.Insert(i);
+            }
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        [Test]
+        public void InsertLeftRightCase() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(25);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(20);
+            root.SetLeft(node1);
+            node1.parent = root;
+    
+            var node2 = new Node<int>(30);
+            root.SetRight(node2);
+            node2.parent = root;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            foreach (var i in new List<int>{30, 20, 25})
+            {
+                actualTree.Insert(i);
+            }
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        [Test]
+        public void InsertRecoloringRelatives() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(25);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(20);
+            root.SetLeft(node1);
+            node1.parent = root;
+            node1.isBlack = true;
+    
+            var node2 = new Node<int>(30);
+            root.SetRight(node2);
+            node2.parent = root;
+            node2.isBlack = true;
+    
+            var node3 = new Node<int>(40);
+            node2.SetRight(node3);
+            node3.parent = node2;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            foreach (var i in new List<int>{20, 25, 30, 40})
+            {
+                actualTree.Insert(i);
+            }
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    
+        /* Insert according to the following scenario:;
+         * 1) Insert root(20) -> recoloring root in black;
+         * 2) Insert node(25), node(30, 30) -> RightRight case;
+         * 3) Insert node(40) -> recoloring father, uncle, grandfather; recoloring root in black;
+         * 4) Insert node(35) -> RightLeft case;
+         * 5) Insert node(27) -> recoloring father, uncle, grandfather;
+         * 6) Insert node(26) -> LeftLeft case;
+         * 7) Insert node(10);
+         * 8) Insert node(15) -> LeftRight case;
+         * 9) Insert node(28) -> recoloring father, uncle, grandfather;;
+         *                           RightLeft case(for grandfather);;
+         * 10) Insert illegal key and varue;
+         */
+        [Test]
+        public void InsertByScenario() {
+    
+            var expectedTree = new Tree<int>();
+    
+            var root = new Node<int>(27);
+            root.isBlack = true;
+    
+            var node1 = new Node<int>(35);
+            root.SetRight(node1);
+            node1.parent = root;
+    
+            var node2 = new Node<int>(30);
+            node1.SetLeft(node2);
+            node2.parent = node1;
+            node2.isBlack = true;
+    
+            var node3 = new Node<int>(40);
+            node1.SetRight(node3);
+            node3.parent = node1;
+            node3.isBlack = true;
+    
+            var node4 = new Node<int>(28);
+            node2.SetLeft(node4);
+            node4.parent = node2;
+    
+            var node5 = new Node<int>(25);
+            root.SetLeft(node5);
+            node5.parent = root;
+    
+            var node6 = new Node<int>(26);
+            node5.SetRight(node6);
+            node6.parent = node5;
+            node6.isBlack = true;
+    
+            var node7 = new Node<int>(15);
+            node5.SetLeft(node7);
+            node7.parent = node5;
+            node7.isBlack = true;
+    
+            var node8 = new Node<int>(20);
+            node7.SetRight(node8);
+            node8.parent = node7;
+    
+            var node9 = new Node<int>(10);
+            node7.SetLeft(node9);
+            node9.parent = node7;
+    
+            expectedTree.root = root;
+    
+            var actualTree = new Tree<int>();
+    
+            foreach (var i in new List<int>{20, 25, 30, 40, 35, 27, 26, 10, 15, 28})
+            {
+                actualTree.Insert(i);
+            }
+    
+            Assert.AreEqual(expectedTree, actualTree);
+    
+        }
+    }
+}
