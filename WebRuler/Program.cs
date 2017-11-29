ef8fe564a2cf8e33bf9d182c22f165325e2cef86
ImmortalTurtle <immortalsuperturtle@gmail.com>
2017-11-29 16:29:08 +0300

    Initialize project for counting length of webpages recursively.

---
+++ WebRuler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebRuler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Someday I will be very important program..");
+        }
+    }
+}
