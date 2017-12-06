899f21292858bcc0848c8dd1c3afc2de4ab75db4
ImmortalTurtle <immortalsuperturtle@gmail.com>
2017-12-06 13:34:12 +0300

    Add RemoveTests to .csproj.

--- RedBlackTree/Tree.cs
+++ RedBlackTree/Tree.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 
 namespace RedBlackTree
 {
