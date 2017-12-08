47b2c562a87024faefd6ffe81c51167339e67617
ImmortalTurtle <immortalsuperturtle@gmail.com>
2017-12-08 09:06:55 +0300

    Test for validity of parallel RBTree.

--- RedBlackTree/Tests/ParallelTreeTests.cs
+++ RedBlackTree/Tests/ParallelTreeTests.cs
@@ -10,50 +10,72 @@ namespace RedBlackTree.Tests
     [TestFixture]
     public class ParallelTreeTests
     {
-        public List<int> InitValues(int amount, bool unique=false)
+        public List<int> InitValues(int amount, bool unique = false)
         {
             var rand = new Random();
             var valuesList = new List<int>();
-            
+
             for (int i = 0; i < amount; i++)
             {
                 valuesList.Add(rand.Next());
             }
             if (!unique)
                 return valuesList;
-            
+
             var uniqueValues = new HashSet<int>(valuesList);
             valuesList = new List<int>(uniqueValues);
-            
-            return valuesList;            
+
+            return valuesList;
         }
-        
+
         [Test]
         public void UniqueInsertsSearchesThenEmpty()
         {
-            var values = InitValues(100000, unique:true);
-            
+            var values = InitValues(100000, unique: true);
+
             var operations = new List<TreeOperation<int>>(500000);
-            
+
             var tree = new ParallelTree<int>(new Tree<int>());
-            
-            foreach(int value in values)
+
+            foreach (int value in values)
             {
                 operations.Add(tree.Insert(value));
             }
-            foreach(int value in values)
+            foreach (int value in values)
             {
                 operations.Add(tree.Find(value));
-            }    
-            foreach(int value in values)
+            }
+            foreach (int value in values)
             {
                 operations.Add(tree.Remove(value));
             }
             tree.Exit(0).Wait();
-            
+
             Assert.AreEqual(new Tree<int>(), tree.GetTree());
-            
+
             Assert.IsTrue(operations.All((operation) => operation.result));
+        }
+
+        [Test]
+        public void InsertsRemovesValidity()
+        {
+            var values = InitValues(10000, unique: false);
+            var parallelTree = new ParallelTree<int>(new Tree<int>());
+            var simpleTree = new Tree<int>();
+
+            foreach (int value in values)
+            {
+                parallelTree.Insert(value);
+                simpleTree.Insert(value);
+            }
+            for (int i = 0; i < 5000; i++)
+            {
+                parallelTree.Remove(values[i]);
+                simpleTree.Remove(values[i]);
+            }
+            parallelTree.Exit(0).Wait();
+
+            Assert.AreEqual(simpleTree, parallelTree.GetTree());
         }
     }
 }
